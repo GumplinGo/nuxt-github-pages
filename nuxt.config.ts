9f8e4393ec8483fbb2fc623ac242623de1038db6
Gumplin Go <guojianglin123@gmail.com>
2024-03-31 13:59:35 +0800

    feat: output folder change to be docs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
+const path = require('path');
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  nitro: {
+    output: {
+      publicDir: path.join(__dirname, 'docs')
+    }
+  },
   // app: {
   //   baseURL: '/nuxt-github-pages/'
   // }
