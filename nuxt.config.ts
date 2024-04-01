a7ba8c7b87a0f646f7ca3d270b6bbd4e90ebc4f6
Gumplin Go <guojianglin123@gmail.com>
2024-04-01 21:05:31 +0800

    feat: add github action

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,11 @@ const path = require('path');
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  nitro: {
-    output: {
-      publicDir: path.join(__dirname, 'docs')
-    }
-  },
+  // nitro: {
+  //   output: {
+  //     publicDir: path.join(__dirname, 'docs')
+  //   }
+  // },
   app: {
     baseURL: '/nuxt-github-pages/',
     buildAssetsDir: 'nuxt_assets',
